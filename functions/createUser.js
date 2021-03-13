ddccacd1872cb4b645255201af9e5a05e39493a2
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-03-13 16:09:42 -0500

    Added createUser Cloud Function infrastructure

--- functions/createUser.js
+++ functions/createUser.js
@@ -1,3 +1,16 @@
-export default createUser = (req, res) => {
-    req.send(req.body);
+const admin = require("firebase-admin");
+
+module.exports = (req, res) => {
+  // Verify the user provides a phone
+  const body = JSON.parse(req.body);
+  if (!body.phone) {
+    return res.status(422).send({error: "Bad Input"});
+  }
+  // Formats the phone number to remove dashes and parens
+  const phone = String(body.phone).replace(/[^\d]/g, "");
+  // Create a new user account using that phone number
+  admin.auth().createUser({uid: phone})
+      .then((user) => res.send(user))
+      .catch((err) => res.status(422).send({error: err}));
+  // Respond to user request with account was made
 };
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const serviceAccount = require("./service-account.json");
-const createUser = require('./createUser');
+const createUser = require("./createUser");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 exports.createUser = functions.https.onRequest(createUser);
